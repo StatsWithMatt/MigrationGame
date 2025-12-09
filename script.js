ac5402b5f8a0f0c4f9f0dd583b625a711c693f42
StatsWithMatt <statswithmatt@gmail.com>
2025-12-09 12:42:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // List of images + instructions
 const options = [
-  { src: "images/option1.jpg", text: "BLUE CHALLENGE - Border Checkpoint" },
-  { src: "images/option2.jpg", text: "YELLOW CHALLENGE - River Crossing" },
-  { src: "images/option3.jpg", text: "RED CHALLENGE - Bribe Smugglers" }
+  { src: "option1.jpg", text: "BLUE CHALLENGE - Border Checkpoint" },
+  { src: "option2.jpg", text: "YELLOW CHALLENGE - River Crossing" },
+  { src: "option3.jpg", text: "RED CHALLENGE - Bribe Smugglers" }
 ];
 
 // Pick a random option
@@ -11,3 +11,4 @@ const randomChoice = options[Math.floor(Math.random() * options.length)];
 // Display it
 document.getElementById("randomImage").src = randomChoice.src;
 document.getElementById("instruction").textContent = randomChoice.text;
+
